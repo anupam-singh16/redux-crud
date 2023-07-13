0fe5601aa29389a51e789765459846f5feac8027
Anupam <anupamsingh16june@gmail.com>
2023-07-13 14:34:36 +0530

    pull from master

---
+++ src/components/To-do.js
@@ -0,0 +1,32 @@
+// import React, { useState } from "react";
+// import { addToDo, deleteToDo, removeToDo } from "../Actions/index";
+// import {  useSelector } from "react-redux";
+import "./to.css"
+
+import To from "./addItem";
+// import List from "./List";
+import Pagination from "./pagination";
+
+
+
+function Todo() {
+  // const [inputData, setInputData] = useState("");
+  // console.log(inputData);
+  // const list = useSelector((state) => state.Reducers.list);
+
+
+  return (
+
+   <div className="main-container"  > 
+      <div className="center-container" > 
+          <To/>
+          <Pagination/>
+          <hr/>   
+      </div>
+   </div>
+
+     
+  )
+}
+
+export default Todo;
